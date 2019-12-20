511473d20885dbf2e4cd7fb05f5ecec14c30e61f
Eleasah Halsmer <eleasahjh@gmail.com>
2019-12-20 11:08:20 -0700

    wip reversing linked list with while loop

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -43,5 +43,11 @@ class LinkedList:
     return False
 
   def reverse_list(self):
-    # TO BE COMPLETED
-    pass
+    # Initialize at head with previous None
+    node = self.head
+    previous = None
+    while node:
+      # store next in temp
+      temp = node.next_node
+      # set next to previous
+      node.next_node = previous
