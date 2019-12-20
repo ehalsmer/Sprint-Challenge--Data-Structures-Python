21f7918599b5708100edc58f2d8903aecc82a44d
Eleasah Halsmer <eleasahjh@gmail.com>
2019-12-20 10:17:05 -0700

    working on passing tests for ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -10,23 +10,46 @@ class RingBuffer:
 
     def append(self, item):
         # Check if size is equal to capacity
+        print('self.size', self.size)
         if self.size < self.capacity:
             self.storage.add_to_tail(item)
             # point new tail at head
             self.storage.tail.next = self.storage.head
+            self.size += 1
         else:
+            print('in else')
             # remove head, add to tail, point new tail at new head (head has been overwritten by new tail)
-            self.storage.remove_from_head
+            self.storage.remove_from_head()
             self.storage.add_to_tail(item)
-            self.storage.tail.next = self.storage.head
+            self.storage.tail.next = self.storage.head.next
+            # self.size += 1
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
-        # TODO: Your code here
+        current = self.storage.head
+        i = 0
+        while i < self.size:
+            i += 1
+            list_buffer_contents.append(current.value)
+            current = current.next
+        #  returns all of the elements in the buffer in a list in their given order.
 
         return list_buffer_contents
+
+
+# myrb = RingBuffer(5)
+# myrb.append('a')
+# myrb.append('b')
+# myrb.append('c')
+# myrb.append('d')
+# myrb.append('e')
+# myrb.append('f')
+# print(myrb.get())
+
+
+
+
 
 # ----------------Stretch Goal-------------------
 
